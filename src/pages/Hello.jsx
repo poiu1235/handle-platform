4c0a97ae9873cdd25997ce9c2e21f82b5e77054f
luisyu <304738837@qq.com>
2026-08-21 19:04:04 +0800

    modify hello.jsx and index.css

--- src/pages/Hello.jsx
+++ src/pages/Hello.jsx
@@ -748,10 +748,7 @@ export default function Hello() {
               checked={includeZero}
               onChange={handleToggleZero}
             />
-            <span className="tgl-btn">
-              <span className="tgl-btn-label tgl-btn-label-off">NO</span>
-              <span className="tgl-btn-label tgl-btn-label-on">YES</span>
-            </span>
+            <span className="tgl-btn" />
             <span className="zero-toggle-label">显示余额为0的小程序列表</span>
           </label>
         </div>
