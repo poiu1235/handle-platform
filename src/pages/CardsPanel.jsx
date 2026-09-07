e1ff24260436ccfc65de195214c7dd085446c45d
luisyu <304738837@qq.com>
2026-09-07 21:38:56 +0800

    modify tips

--- src/pages/CardsPanel.jsx
+++ src/pages/CardsPanel.jsx
@@ -1657,7 +1657,7 @@ function CardAddModal({ today, onClose, onCreated }) {
             />
           </div>
           <div className="bd-field">
-            <label>{renewOn ? '终止日期（= 扣款日）' : '终止日期（不填 = 最长 2 年）'}</label>
+            <label>{renewOn ? '终止日期（= 扣款日）' : '终止日期（不填 = 最长2年）'}</label>
             <input
               type="date"
               value={renewOn ? billingDate : endDate}
